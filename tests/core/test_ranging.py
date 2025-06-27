348ced0f33e806fcf67f456f037702dc41fedd0a
Brett Gottula <brett@cascade.space>
2025-06-27 11:57:41 -0700

    Work on ranging.py unit tests

--- tests/core/test_ranging.py
+++ tests/core/test_ranging.py
@@ -1,25 +1,47 @@
-"""Tests for the ranging module."""
+"""Tests for the ranging module.
+
+See ranging.py for references.
+"""
 
 import pytest
 import astropy.units as u
 from astropy.tests.helper import assert_quantity_allclose
 
-from src.spacelink.core.ranging import pn_sequence_range_ambiguity
-from src.spacelink.core.units import Frequency, Distance
+from spacelink.core.ranging import pn_sequence_range_ambiguity, chip_snr
+from spacelink.core.units import Frequency, Distance, DecibelHertz, Decibels
 
 
 # DO NOT MODIFY
 @pytest.mark.parametrize(
     "chip_rate, expected_ambiguity",
     [
-        # CCSDS 414.0-G-2, pg 2-2
-        (1.0 * u.MHz, 75710 * u.km),
+        # [1] p. 13.
+        #
+        # [3] has the same example on page 2-2, but they were evidently lazy and used
+        # 3e8 for speed of light so the approximate range ambiguity they give (75710 km)
+        # is less accurate.
+        (1.0 * u.MHz, 75660 * u.km),
     ],
 )
-def test_pn_sequence_range_ambiguity_valid(
+def test_pn_sequence_range_ambiguity(
     chip_rate: Frequency, expected_ambiguity: Distance
 ):
-    """Test pn_sequence_range_ambiguity with valid inputs."""
     ambiguity = pn_sequence_range_ambiguity(chip_rate)
-    # Use a relative tolerance because the numbers can be large
-    assert_quantity_allclose(ambiguity, expected_ambiguity, rtol=1e-3)
+    # Using loose tolerance because the example is approximate
+    assert_quantity_allclose(ambiguity, expected_ambiguity, rtol=1e-4)
+
+
+@pytest.mark.parametrize(
+    "ranging_clock_rate, prn0, expected_chip_snr",
+    [
+        # [3] p. 2-3.
+        (1.0 * u.MHz, 27 * u.dBHz, -33 * u.dB),
+    ],
+)
+def test_chip_snr(
+    ranging_clock_rate: Frequency, prn0: DecibelHertz, expected_chip_snr: Decibels
+):
+    chip_snr_result = chip_snr(ranging_clock_rate, prn0)
+    # TODO: There is a subtle unit incompatibility here, requiring the use of 
+    # `.to(u.dB)` to make `assert_quantity_allclose()` happy.
+    assert_quantity_allclose(chip_snr_result.to(u.dB), expected_chip_snr)
