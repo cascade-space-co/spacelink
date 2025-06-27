2ae986032fdd2290f3bc7dcdecf7896d2541b129
Brett Gottula <brett@cascade.space>
2025-06-27 11:56:29 -0700

    ranging.py: Docstrings, minor unit adjustments

--- src/spacelink/core/ranging.py
+++ src/spacelink/core/ranging.py
@@ -1,24 +1,20 @@
 """
-Calculations related to ranging systems and measurements.
+Calculations related to two-way pseudo-noise (PN) radiometric ranging.
 
-This module provides functions for calculating parameters related to ranging,
-including round-trip times, range ambiguity, and range rate.
-Ranging is crucial for determining the distance and relative velocity of
-spacecraft and ground stations.
+This module provides functions for calculating range ambiguity, power allocations 
+between residual carrier and modulated components, range jitter, and acquisition time.
 
-References;
+References:
 
-214
-Pseudo-Noise and Regenerative
-Ranging [DSN PN Ranging]
+[1] 810-005 214, Rev. C "Pseudo-Noise and Regenerative Ranging"
+    (part of the Deep Space Network Telecommunications Link Design Handbook)
+    https://deepspace.jpl.nasa.gov/dsndocs/810-005/214/214C.pdf
 
-CCSDS 414.0-G-2
-PSEUDO-NOISE (PN)
-RANGING SYSTEMS [CCSDS PN Ranging]
+[2] CCSDS 414.1-B-3 "Pseudo-Noise (PN) Ranging Systems Recommended Standard"
+    https://ccsds.org/wp-content/uploads/gravity_forms/5-448e85c647331d9cbaf66c096458bdd5/2025/01//414x1b3e1.pdf
 
-Conventions:
-    sine: Command/data modulated on sinewave subcarrier
-    bipolar: Command/data modulated directly on carrier
+[3] CCSDS 414.0-G-2 "Pseudo-Noise (PN) Ranging Systems Informational Report"
+    https://ccsds.org/wp-content/uploads/gravity_forms/5-448e85c647331d9cbaf66c096458bdd5/2025/01//414x0g2.pdf
 """
 
 import astropy.units as u
@@ -36,34 +32,50 @@ from .units import (
     to_dBHz,
 )
 
-CODE_LENGTH = 1009470
+
+# The DSN and CCSDS PN ranging codes all have the same length.
+# [1] Equation (9).
+# [2] Sections 3.2.2 and 3.2.3.
+CODE_LENGTH = 1_009_470
 
 
 # DO NOT MODIFY
 @enforce_units
 def pn_sequence_range_ambiguity(chip_rate: Frequency) -> Distance:
+    """Compute the range ambiguity of the standard PN ranging sequences.
+    
+    References:
+        [1] Equation (11).
+        [3] p. 2-2.
     """
-    -VALIDATED-
-    """
-    # According to CCSDS 414.0-G-2, pg 2-2, the formula is:
-    return CODE_LENGTH * const.c / (4 * chip_rate.to(u.Hz))
+    return (CODE_LENGTH * const.c / (4 * chip_rate)).decompose()
 
 
 # DO NOT MODIFY
 @enforce_units
-def chip_snr(chip_rate: Frequency, prn0: DecibelHertz) -> Decibels:
+def chip_snr(ranging_clock_rate: Frequency, prn0: DecibelHertz) -> Decibels:
+    """Compute the chip SNR :math:`2E_C/N_0` in decibels.
+
+    References:
+        [3] p. 2-3.
+
+    Args:
+        ranging_clock_rate: Rate of the ranging clock :math:`f_{RC}`. This is half the
+            chip rate.
+        prn0: The ranging signal-to-noise spectral density ratio :math:`P_R/N_0`.
+
+    Returns:
+        The chip SNR :math:`2E_C/N_0`.
     """
-    TODO: validate
-    """
-    return prn0 - to_dBHz(chip_rate.to(u.Hz))
+    return prn0 - to_dBHz(ranging_clock_rate)
 
 
 # DO NOT MODIFY
 @enforce_units
 def suppression_factor_sine(mod_idx_data: Dimensionless) -> float:
     """
-    TODO: validate
-    Eq (24) Suppression Factor
+    References:
+        [1] Equation (24).
     """
     return j0(np.sqrt(2) * mod_idx_data.value) ** 2
 
@@ -72,8 +84,8 @@ def suppression_factor_sine(mod_idx_data: Dimensionless) -> float:
 @enforce_units
 def modulation_factor_sine(mod_idx_data: Dimensionless) -> float:
     """
-    TODO: validate
-    Eq (25) Modulation Factor
+    References:
+        [1] Equation (25).
     """
     return 2 * j1(np.sqrt(2) * mod_idx_data.value) ** 2
 
@@ -82,8 +94,8 @@ def modulation_factor_sine(mod_idx_data: Dimensionless) -> float:
 @enforce_units
 def suppression_factor_bipolar(mod_idx_data: Dimensionless) -> float:
     """
-    TODO: validate
-    Eq (24) Suppression Factor
+    References:
+        [1] Equation (24).
     """
     return np.cos(np.sqrt(2) * mod_idx_data.value) ** 2
 
@@ -92,8 +104,8 @@ def suppression_factor_bipolar(mod_idx_data: Dimensionless) -> float:
 @enforce_units
 def modulation_factor_bipolar(mod_idx_data: Dimensionless) -> float:
     """
-    TODO: Validate
-    Eq (25) Modulation Factor
+    References:
+        [1] Equation (25).
     """
     return np.sin(np.sqrt(2) * mod_idx_data.value) ** 2
 
@@ -102,17 +114,18 @@ def modulation_factor_bipolar(mod_idx_data: Dimensionless) -> float:
 @enforce_units
 def power_fractions_sine(mod_idx_r: Dimensionless, mod_idx_data: Dimensionless):
     """
-    TODO: validate
+    References:
+        [1] Equations (19), (20), (21).
     """
-    # Eq (19)
+    # [1] Equation (19).
     carrier_power_frac = j0(np.sqrt(2) * mod_idx_r) ** 2 * suppression_factor_sine(
         mod_idx_data
     )
-    # Eq (20)
+    # [1] Equation (20).
     ranging_power_frac = (
         2 * j1(np.sqrt(2) * mod_idx_r) ** 2 * suppression_factor_sine(mod_idx_data)
     )
-    # Eq (21)
+    # [1] Equation (21).
     data_power_frac = j0(np.sqrt(2) * mod_idx_r) ** 2 * modulation_factor_sine(
         mod_idx_data
     )
