dc287e9e0d26d5c2550afed6d9ca6c515b4813dc
Brett Gottula <bgottula@gmail.com>
2025-06-28 21:22:33 -0700

    Ranging work (#16)
    Still have some unit tests to write.
    
    Co-Authored-By: Jacob Portukalian <jportukalian@gmail.com>

--- src/spacelink/core/ranging.py
+++ src/spacelink/core/ranging.py
@@ -1,27 +1,26 @@
 """
-Calculations related to ranging systems and measurements.
+Calculations related to two-way pseudo-noise (PN) radiometric ranging.
 
-This module provides functions for calculating parameters related to ranging,
-including round-trip times, range ambiguity, and range rate.
-Ranging is crucial for determining the distance and relative velocity of
-spacecraft and ground stations.
+This module provides functions for calculating range ambiguity and power allocations
+between residual carrier and modulated components.
 
-References;
+References
+----------
+[1] 810-005 214, Rev. C "Pseudo-Noise and Regenerative Ranging"
+    (part of the Deep Space Network Telecommunications Link Design Handbook)
+    https://deepspace.jpl.nasa.gov/dsndocs/810-005/214/214C.pdf
 
-214
-Pseudo-Noise and Regenerative
-Ranging [DSN PN Ranging]
+[2] CCSDS 414.1-B-3 "Pseudo-Noise (PN) Ranging Systems Recommended Standard"
+    https://ccsds.org/wp-content/uploads/gravity_forms/5-448e85c647331d9cbaf66c096458bdd5/2025/01//414x1b3e1.pdf
 
-CCSDS 414.0-G-2
-PSEUDO-NOISE (PN)
-RANGING SYSTEMS [CCSDS PN Ranging]
-
-Conventions:
-    sine: Command/data modulated on sinewave subcarrier
-    bipolar: Command/data modulated directly on carrier
+[3] CCSDS 414.0-G-2 "Pseudo-Noise (PN) Ranging Systems Informational Report"
+    https://ccsds.org/wp-content/uploads/gravity_forms/5-448e85c647331d9cbaf66c096458bdd5/2025/01//414x0g2.pdf
 """
 
+import enum
+import math
 import astropy.units as u
+from astropy.coordinates import Angle
 import astropy.constants as const
 import numpy as np
 from scipy.special import j0, j1
@@ -36,85 +35,228 @@ from .units import (
     to_dBHz,
 )
 
-CODE_LENGTH = 1009470
+
+class CommandMod(enum.Enum):
+    """The type of command modulation used alongside ranging on the uplink."""
+
+    BIPOLAR = enum.auto()
+    SINE_SUBCARRIER = enum.auto()
+
+
+# The DSN and CCSDS PN ranging codes all have the same length.
+# [1] Equation (9).
+# [2] Sections 3.2.2 and 3.2.3.
+CODE_LENGTH = 1_009_470
 
 
 # DO NOT MODIFY
 @enforce_units
-def pn_sequence_range_ambiguity(chip_rate: Frequency) -> Distance:
+def pn_sequence_range_ambiguity(ranging_clock_rate: Frequency) -> Distance:
+    r"""
+    Compute the range ambiguity of the standard PN ranging sequences.
+
+    Parameters
+    ----------
+    ranging_clock_rate : Frequency
+        Rate of the ranging clock :math:`f_{RC}`. This is half the chip rate.
+
+    Returns
+    -------
+    Distance
+        The range ambiguity distance.
+
+    References
+    ----------
+    [1] Equation (11).
+    [3] p. 2-2.
     """
-    -VALIDATED-
-    """
-    # According to CCSDS 414.0-G-2, pg 2-2, the formula is:
-    return CODE_LENGTH * const.c / (4 * chip_rate.to(u.Hz))
+    return (CODE_LENGTH * const.c / (4 * ranging_clock_rate)).decompose()
 
 
 # DO NOT MODIFY
 @enforce_units
-def chip_snr(chip_rate: Frequency, prn0: DecibelHertz) -> Decibels:
+def chip_snr(ranging_clock_rate: Frequency, prn0: DecibelHertz) -> Decibels:
+    r"""
+    Compute the chip SNR :math:`2E_C/N_0` in decibels.
+
+    Parameters
+    ----------
+    ranging_clock_rate : Frequency
+        Rate of the ranging clock :math:`f_{RC}`. This is half the chip rate.
+    prn0 : DecibelHertz
+        The ranging signal-to-noise spectral density ratio :math:`P_R/N_0`.
+
+    Returns
+    -------
+    Decibels
+        The chip SNR :math:`2E_C/N_0`.
+
+    References
+    ----------
+    [3] p. 2-3.
     """
-    TODO: validate
-    """
-    return prn0 - to_dBHz(chip_rate.to(u.Hz))
+    return prn0 - to_dBHz(ranging_clock_rate)
 
 
 # DO NOT MODIFY
 @enforce_units
-def suppression_factor_sine(mod_idx_data: Dimensionless) -> float:
+def _suppression_factor(mod_idx: Angle, modulation: CommandMod) -> Dimensionless:
+    r"""
+    Compute the suppression factor :math:`S_{cmd}(\phi_{cmd})`.
+
+    This is used in the expressions for uplink carrier and ranging power fractions.
+
+    Parameters
+    ----------
+    mod_idx : Angle
+        The RMS phase deviation by command signal :math:`\phi_{cmd}`.
+    modulation : CommandModulation
+        The command modulation type.
+
+    Returns
+    -------
+    Dimensionless
+        The suppression factor :math:`S_{cmd}(\phi_{cmd})`.
+
+    References
+    ----------
+    [1] Equation (24).
     """
-    TODO: validate
-    Eq (24) Suppression Factor
-    """
-    return j0(np.sqrt(2) * mod_idx_data.value) ** 2
+    mod_idx_rad = mod_idx.to(u.rad).value
+    if modulation == CommandMod.BIPOLAR:
+        return np.cos(mod_idx_rad) ** 2
+    elif modulation == CommandMod.SINE_SUBCARRIER:
+        return j0(math.sqrt(2) * mod_idx_rad) ** 2
+    else:
+        raise ValueError(f"Invalid command modulation type: {modulation}")
 
 
 # DO NOT MODIFY
 @enforce_units
-def modulation_factor_sine(mod_idx_data: Dimensionless) -> float:
+def _modulation_factor(mod_idx: Angle, modulation: CommandMod) -> Dimensionless:
+    r"""
+    Compute the modulation factor :math:`M_{cmd}(\phi_{cmd})`.
+
+    This is used in the expression for uplink data power fraction.
+
+    Parameters
+    ----------
+    mod_idx : Angle
+        The RMS phase deviation by command signal :math:`\phi_{cmd}`.
+    modulation : CommandModulation
+        The command modulation type.
+
+    Returns
+    -------
+    Dimensionless
+        The modulation factor :math:`M_{cmd}(\phi_{cmd})`.
+
+    References
+    ----------
+    [1] Equation (25).
     """
-    TODO: validate
-    Eq (25) Modulation Factor
-    """
-    return 2 * j1(np.sqrt(2) * mod_idx_data.value) ** 2
+    mod_idx_rad = mod_idx.to(u.rad).value
+    if modulation == CommandMod.BIPOLAR:
+        return np.sin(mod_idx_rad) ** 2
+    elif modulation == CommandMod.SINE_SUBCARRIER:
+        return 2 * j1(math.sqrt(2) * mod_idx_rad) ** 2
+    else:
+        raise ValueError(f"Invalid command modulation type: {modulation}")
 
 
-# DO NOT MODIFY
 @enforce_units
-def suppression_factor_bipolar(mod_idx_data: Dimensionless) -> float:
+def uplink_carrier_to_total_power(
+    mod_idx_ranging: Angle,
+    mod_idx_cmd: Angle,
+    modulation: CommandMod,
+) -> Dimensionless:
+    r"""
+    Uplink ratio of residual carrier power to total power :math:`P_{C}/P_{T}`.
+
+    Parameters
+    ----------
+    mod_idx_ranging : Angle
+        The RMS phase deviation by ranging signal :math:`\phi_{r}`.
+    mod_idx_cmd : Angle
+        The RMS phase deviation by command signal :math:`\phi_{cmd}`.
+    modulation : CommandModulation
+        The command modulation type.
+
+    Returns
+    -------
+    Dimensionless
+        The ratio of residual carrier power to total power :math:`P_{C}/P_{T}`.
+
+    References
+    ----------
+    [1] Equation (20).
     """
-    TODO: validate
-    Eq (24) Suppression Factor
-    """
-    return np.cos(np.sqrt(2) * mod_idx_data.value) ** 2
+    return j0(
+        math.sqrt(2) * mod_idx_ranging.to(u.rad).value
+    ) ** 2 * _suppression_factor(mod_idx_cmd, modulation)
 
 
-# DO NOT MODIFY
 @enforce_units
-def modulation_factor_bipolar(mod_idx_data: Dimensionless) -> float:
-    """
-    TODO: Validate
-    Eq (25) Modulation Factor
-    """
-    return np.sin(np.sqrt(2) * mod_idx_data.value) ** 2
+def uplink_ranging_to_total_power(
+    mod_idx_ranging: Angle,
+    mod_idx_cmd: Angle,
+    modulation: CommandMod,
+) -> Dimensionless:
+    r"""
+    Uplink ratio of usable ranging power to total power :math:`P_{R}/P_{T}`.
 
+    Parameters
+    ----------
+    mod_idx_ranging : Angle
+        The RMS phase deviation by ranging signal :math:`\phi_{r}`.
+    mod_idx_cmd : Angle
+        The RMS phase deviation by command signal :math:`\phi_{cmd}`.
+    modulation : CommandModulation
+        The command modulation type.
 
-# DO NOT MODIFY
-@enforce_units
-def power_fractions_sine(mod_idx_r: Dimensionless, mod_idx_data: Dimensionless):
+    Returns
+    -------
+    Dimensionless
+        The ratio of usable ranging power to total power :math:`P_{R}/P_{T}`.
+
+    References
+    ----------
+    [1] Equation (20).
     """
-    TODO: validate
-    """
-    # Eq (19)
-    carrier_power_frac = j0(np.sqrt(2) * mod_idx_r) ** 2 * suppression_factor_sine(
-        mod_idx_data
+    return (
+        2
+        * j1(math.sqrt(2) * mod_idx_ranging.to(u.rad).value) ** 2
+        * _suppression_factor(mod_idx_cmd, modulation)
     )
-    # Eq (20)
-    ranging_power_frac = (
-        2 * j1(np.sqrt(2) * mod_idx_r) ** 2 * suppression_factor_sine(mod_idx_data)
-    )
-    # Eq (21)
-    data_power_frac = j0(np.sqrt(2) * mod_idx_r) ** 2 * modulation_factor_sine(
-        mod_idx_data
-    )
 
-    return carrier_power_frac, ranging_power_frac, data_power_frac
+
+@enforce_units
+def uplink_data_to_total_power(
+    mod_idx_ranging: Angle,
+    mod_idx_cmd: Angle,
+    modulation: CommandMod,
+) -> Dimensionless:
+    r"""
+    Uplink ratio of usable data power to total power :math:`P_{D}/P_{T}`.
+
+    Parameters
+    ----------
+    mod_idx_ranging : Angle
+        The RMS phase deviation by ranging signal :math:`\phi_{r}`.
+    mod_idx_cmd : Angle
+        The RMS phase deviation by command signal :math:`\phi_{cmd}`.
+    modulation : CommandModulation
+        The command modulation type.
+
+    Returns
+    -------
+    Dimensionless
+        The ratio of usable data power to total power :math:`P_{D}/P_{T}`.
+
+    References
+    ----------
+    [1] Equation (21).
+    """
+    return j0(math.sqrt(2) * mod_idx_ranging.to(u.rad).value) ** 2 * _modulation_factor(
+        mod_idx_cmd, modulation
+    )
--- tests/core/test_ranging.py
+++ tests/core/test_ranging.py
@@ -1,25 +1,186 @@
-"""Tests for the ranging module."""
+"""Tests for the ranging module.
+
+See ranging.py for references.
+"""
 
 import pytest
 import astropy.units as u
 from astropy.tests.helper import assert_quantity_allclose
+from astropy.coordinates import Angle
+import numpy as np
 
-from src.spacelink.core.ranging import pn_sequence_range_ambiguity
-from src.spacelink.core.units import Frequency, Distance
+from spacelink.core import ranging
+from spacelink.core.units import (
+    Frequency,
+    Distance,
+    DecibelHertz,
+    Decibels,
+    Dimensionless,
+)
 
 
 # DO NOT MODIFY
 @pytest.mark.parametrize(
-    "chip_rate, expected_ambiguity",
+    "ranging_clock_rate, expected_ambiguity",
     [
-        # CCSDS 414.0-G-2, pg 2-2
-        (1.0 * u.MHz, 75710 * u.km),
+        # [1] p. 13.
+        #
+        # [3] has the same example on page 2-2, but they were evidently lazy and used
+        # 3e8 for speed of light so the approximate range ambiguity they give (75710 km)
+        # is less accurate.
+        (1.0 * u.MHz, 75660 * u.km),
     ],
 )
-def test_pn_sequence_range_ambiguity_valid(
-    chip_rate: Frequency, expected_ambiguity: Distance
+def test_pn_sequence_range_ambiguity(
+    ranging_clock_rate: Frequency, expected_ambiguity: Distance
 ):
-    """Test pn_sequence_range_ambiguity with valid inputs."""
-    ambiguity = pn_sequence_range_ambiguity(chip_rate)
-    # Use a relative tolerance because the numbers can be large
-    assert_quantity_allclose(ambiguity, expected_ambiguity, rtol=1e-3)
+    ambiguity = ranging.pn_sequence_range_ambiguity(ranging_clock_rate)
+    # Using loose tolerance because the example is approximate
+    assert_quantity_allclose(ambiguity, expected_ambiguity, rtol=1e-4)
+
+
+@pytest.mark.parametrize(
+    "ranging_clock_rate, prn0, expected_chip_snr",
+    [
+        # [3] p. 2-3.
+        (1.0 * u.MHz, 27 * u.dBHz, -33 * u.dB),
+    ],
+)
+def test_chip_snr(
+    ranging_clock_rate: Frequency, prn0: DecibelHertz, expected_chip_snr: Decibels
+):
+    chip_snr_result = ranging.chip_snr(ranging_clock_rate, prn0)
+    # TODO: There is a subtle unit incompatibility here, requiring the use of
+    # `.to(u.dB)` to make `assert_quantity_allclose()` happy.
+    assert_quantity_allclose(chip_snr_result.to(u.dB), expected_chip_snr)
+
+
+@pytest.mark.parametrize(
+    "mod_idx, modulation, expected_suppression",
+    [
+        # No suppression expected when mod index is 0.
+        (0.0 * u.rad, ranging.CommandMod.BIPOLAR, 1.0),
+        (0.0 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 1.0),
+        # Special cases for bipolar modulation.
+        (np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5),
+        (np.pi / 2 * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        (np.pi * u.rad, ranging.CommandMod.BIPOLAR, 1.0),
+        # Some hand-picked cases for sine subcarrier modulation computed using
+        # WolframAlpha.
+        (0.5 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.772382),
+        (1.0 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.312631),
+    ],
+)
+def test_suppression_factor(
+    mod_idx: Angle, modulation: ranging.CommandMod, expected_suppression: Dimensionless
+):
+    suppression = ranging._suppression_factor(mod_idx, modulation)
+    assert_quantity_allclose(suppression, expected_suppression, atol=1e-10, rtol=1e-6)
+
+
+@pytest.mark.parametrize(
+    "mod_idx, modulation, expected_modulation_factor",
+    [
+        # No modulation expected when mod index is 0.
+        (0.0 * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        (0.0 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.0),
+        # Special cases for bipolar modulation.
+        (np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5),
+        (np.pi / 2 * u.rad, ranging.CommandMod.BIPOLAR, 1.0),
+        (np.pi * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        # Some hand-picked cases for sine subcarrier modulation computed using
+        # WolframAlpha.
+        (0.5 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.220331),
+        (1.0 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.592879),
+    ],
+)
+def test_modulation_factor(
+    mod_idx: Angle,
+    modulation: ranging.CommandMod,
+    expected_modulation_factor: Dimensionless,
+):
+    modulation_factor = ranging._modulation_factor(mod_idx, modulation)
+    assert_quantity_allclose(
+        modulation_factor, expected_modulation_factor, atol=1e-10, rtol=1e-6
+    )
+
+
+@pytest.mark.parametrize(
+    "mod_idx_ranging, mod_idx_cmd, modulation, expected_carrier_ratio",
+    [
+        # No modulation (all power in carrier)
+        (0.0 * u.rad, 0.0 * u.rad, ranging.CommandMod.BIPOLAR, 1.0),
+        # Ranging only
+        (0.5 * u.rad, 0.0 * u.rad, ranging.CommandMod.BIPOLAR, 0.772382),
+        # Data modulation only
+        (0.0 * u.rad, np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5),
+        # Simultaneous ranging and data modulation, bipolar
+        (0.5 * u.rad, np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5 * 0.772382),
+        # Simultaneous ranging and data modulation, sine subcarrier
+        (0.5 * u.rad, 0.5 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.772382**2),
+    ],
+)
+def test_uplink_carrier_to_total_power(
+    mod_idx_ranging: Angle,
+    mod_idx_cmd: Angle,
+    modulation: ranging.CommandMod,
+    expected_carrier_ratio: Dimensionless,
+):
+    carrier_ratio = ranging.uplink_carrier_to_total_power(
+        mod_idx_ranging, mod_idx_cmd, modulation
+    )
+    assert_quantity_allclose(carrier_ratio, expected_carrier_ratio, rtol=1e-4)
+
+
+@pytest.mark.parametrize(
+    "mod_idx_ranging, mod_idx_cmd, modulation, expected_ranging_ratio",
+    [
+        # No modulation (all power in carrier)
+        (0.0 * u.rad, 0.0 * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        # Ranging only
+        (0.5 * u.rad, 0.0 * u.rad, ranging.CommandMod.BIPOLAR, 0.220331),
+        # Data modulation only
+        (0.0 * u.rad, np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        # Simultaneous ranging and data modulation, bipolar
+        (0.5 * u.rad, np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5 * 0.220331),
+        # Simultaneous ranging and data modulation, sine subcarrier
+        (0.5 * u.rad, 0.5 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.170180),
+    ],
+)
+def test_uplink_ranging_to_total_power(
+    mod_idx_ranging: Angle,
+    mod_idx_cmd: Angle,
+    modulation: ranging.CommandMod,
+    expected_ranging_ratio: Dimensionless,
+):
+    ranging_ratio = ranging.uplink_ranging_to_total_power(
+        mod_idx_ranging, mod_idx_cmd, modulation
+    )
+    assert_quantity_allclose(ranging_ratio, expected_ranging_ratio, rtol=1e-4)
+
+
+@pytest.mark.parametrize(
+    "mod_idx_ranging, mod_idx_cmd, modulation, expected_data_ratio",
+    [
+        # No modulation (all power in carrier)
+        (0.0 * u.rad, 0.0 * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        # Ranging only
+        (0.5 * u.rad, 0.0 * u.rad, ranging.CommandMod.BIPOLAR, 0.0),
+        # Data modulation only
+        (0.0 * u.rad, np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5),
+        # Simultaneous ranging and data modulation, bipolar
+        (0.5 * u.rad, np.pi / 4 * u.rad, ranging.CommandMod.BIPOLAR, 0.5 * 0.772382),
+        # Simultaneous ranging and data modulation, sine subcarrier
+        (0.5 * u.rad, 0.5 * u.rad, ranging.CommandMod.SINE_SUBCARRIER, 0.170180),
+    ],
+)
+def test_uplink_data_to_total_power(
+    mod_idx_ranging: Angle,
+    mod_idx_cmd: Angle,
+    modulation: ranging.CommandMod,
+    expected_data_ratio: Dimensionless,
+):
+    data_ratio = ranging.uplink_data_to_total_power(
+        mod_idx_ranging, mod_idx_cmd, modulation
+    )
+    assert_quantity_allclose(data_ratio, expected_data_ratio, rtol=1e-4)
