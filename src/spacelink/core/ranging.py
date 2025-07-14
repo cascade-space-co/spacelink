2c2f38024c8bb570a86c1d1799ab2b5242b31776
Brett Gottula <brett@cascade.space>
2025-07-14 11:54:22 -0700

    Add PN ranging acquisition probability calculations (#29)

--- src/spacelink/core/ranging.py
+++ src/spacelink/core/ranging.py
@@ -27,7 +27,8 @@ import math
 import astropy.units as u
 import astropy.constants as const
 import numpy as np
-from scipy.special import j0, j1
+import scipy.integrate
+import scipy.special
 
 from .units import (
     Angle,
@@ -36,8 +37,17 @@ from .units import (
     Dimensionless,
     Frequency,
     Distance,
+    Time,
     enforce_units,
 )
+
+
+class PnRangingCode(enum.Enum):
+    """The type of PN ranging code used."""
+
+    DSN = enum.auto()
+    CCSDS_T4B = enum.auto()
+    CCSDS_T2B = enum.auto()
 
 
 class DataModulation(enum.Enum):
@@ -47,10 +57,40 @@ class DataModulation(enum.Enum):
     SINE_SUBCARRIER = enum.auto()
 
 
-# The DSN and CCSDS PN ranging codes all have the same length.
-# [2] Equation (9).
-# [3] Sections 3.2.2 and 3.2.3.
 CODE_LENGTH = 1_009_470
+"""int: The length of the full PN ranging sequence.
+
+The DSN and CCSDS PN ranging codes all have the same length.
+
+References
+----------
+`[2]`_ Equation (9).
+
+`[3]`_ Sections 3.2.2 and 3.2.3.
+"""
+
+
+COMPONENT_LENGTHS = {
+    1: 2,
+    2: 7,
+    3: 11,
+    4: 15,
+    5: 19,
+    6: 23,
+}
+"""dict[int, int]: The lengths of the six components of the PN ranging codes.
+
+The DSN and CCSDS PN ranging codes all have the same component lengths.
+
+The key is the component index (1-6).
+The value is the length of the component in chips.
+
+References
+----------
+`[2]`_ Table 2.
+
+`[3]`_ Sections 3.2.2 and 3.2.3.
+"""
 
 
 @enforce_units
@@ -130,7 +170,7 @@ def _suppression_factor(mod_idx: Angle, modulation: DataModulation) -> Dimension
     if modulation == DataModulation.BIPOLAR:
         suppression_factor = np.cos(mod_idx_rad) ** 2
     elif modulation == DataModulation.SINE_SUBCARRIER:
-        suppression_factor = j0(math.sqrt(2) * mod_idx_rad) ** 2
+        suppression_factor = scipy.special.j0(math.sqrt(2) * mod_idx_rad) ** 2
     else:
         raise ValueError(f"Invalid data modulation type: {modulation}")
     return suppression_factor * u.dimensionless_unscaled
@@ -165,7 +205,7 @@ def _modulation_factor(mod_idx: Angle, modulation: DataModulation) -> Dimensionl
     if modulation == DataModulation.BIPOLAR:
         mod_factor = np.sin(mod_idx_rad) ** 2
     elif modulation == DataModulation.SINE_SUBCARRIER:
-        mod_factor = 2 * j1(math.sqrt(2) * mod_idx_rad) ** 2
+        mod_factor = 2 * scipy.special.j1(math.sqrt(2) * mod_idx_rad) ** 2
     else:
         raise ValueError(f"Invalid data modulation type: {modulation}")
     return mod_factor * u.dimensionless_unscaled
@@ -211,7 +251,7 @@ def carrier_to_total_power(
     `[2]`_ Equation (19) for PN ranging uplink, (50) for regenerative downlink.
     """
     return (
-        j0(math.sqrt(2) * mod_idx_ranging.value) ** 2
+        scipy.special.j0(math.sqrt(2) * mod_idx_ranging.value) ** 2
         * _suppression_factor(mod_idx_data, modulation)
     ) * u.dimensionless_unscaled
 
@@ -257,7 +297,7 @@ def ranging_to_total_power(
     """
     return (
         2
-        * j1(math.sqrt(2) * mod_idx_ranging.value) ** 2
+        * scipy.special.j1(math.sqrt(2) * mod_idx_ranging.value) ** 2
         * _suppression_factor(mod_idx_data, modulation)
     ) * u.dimensionless_unscaled
 
@@ -302,6 +342,139 @@ def data_to_total_power(
     `[2]`_ Equation (21) for PN ranging uplink, (52) for regenerative downlink.
     """
     return (
-        j0(math.sqrt(2) * mod_idx_ranging.value) ** 2
+        scipy.special.j0(math.sqrt(2) * mod_idx_ranging.value) ** 2
         * _modulation_factor(mod_idx_data, modulation)
     ) * u.dimensionless_unscaled
+
+
+# Values of the cross-correlation factors R_n given in Tables 3, 4, and 5 of [2].
+_CORR_COEFF_DSN = {
+    # [2] Table 3
+    PnRangingCode.DSN: {
+        1: 0.9544,
+        2: 0.0456,
+        3: 0.0456,
+        4: 0.0456,
+        5: 0.0456,
+        6: 0.0456,
+    },
+    # [2] Table 4
+    PnRangingCode.CCSDS_T4B: {
+        1: 0.9387,
+        2: 0.0613,
+        3: 0.0613,
+        4: 0.0613,
+        5: 0.0613,
+        6: 0.0613,
+    },
+    # [2] Table 5
+    PnRangingCode.CCSDS_T2B: {
+        1: 0.6274,
+        2: 0.2447,
+        3: 0.2481,
+        4: 0.2490,
+        5: 0.2492,
+        6: 0.2496,
+    },
+}
+
+
+@enforce_units
+def pn_component_acquisition_probability(
+    ranging_to_noise_psd: Frequency,
+    integration_time: Time,
+    code: PnRangingCode,
+    component: int,
+) -> Dimensionless:
+    r"""
+    Compute the acquisition probability for one component of the PN ranging code.
+
+    Successful acquisition means that the correct phase of the component code is
+    identified by the receiver. The number of possible phases is equal to the component
+    length.
+
+    This calculation assumes that the received signal is correlated against all
+    possible cyclic shifts of the component code in parallel and that the shift with the
+    maximum correlation output is selected. See `[4]`_ Section 2.6.2 for details.
+
+    This function uses equation (91) from `[2]`_. `[4]`_ has an equivalent equation in
+    Section 2.6.3.2, but it uses slightly different values for the cross-correlation
+    coefficients which leads to slightly different acquisition probabilities.
+
+    Parameters
+    ----------
+    ranging_to_noise_psd : Frequency
+        The ranging-to-noise power spectral density :math:`P_R/N_0`.
+    integration_time : Time
+        The integration time :math:`T`.
+    code : PnRangingCode
+        The PN ranging code type.
+    component : int
+        The component index in [1, 6].
+
+    Returns
+    -------
+    Dimensionless
+        The acquisition probability for the given component.
+
+    References
+    ----------
+    `[2]`_ Equation (91).
+    """
+    comp_len = COMPONENT_LENGTHS[component]
+    corr_coeff = _CORR_COEFF_DSN[code][component]
+    corr_term = corr_coeff * math.sqrt(integration_time * ranging_to_noise_psd)
+    integral = scipy.integrate.quad(
+        lambda x: (
+            math.exp(-(x**2))
+            * ((1 + scipy.special.erf(x + corr_term)) / 2) ** (comp_len - 1)
+        ),
+        -np.inf,
+        np.inf,
+    )[0]
+    return integral / math.sqrt(np.pi) * u.dimensionless
+
+
+@enforce_units
+def pn_acquisition_probability(
+    ranging_to_noise_psd: Frequency, integration_time: Time, code: PnRangingCode
+) -> Dimensionless:
+    r"""
+    Compute the overall acquisition probability for the PN ranging code.
+
+    This is the product of the acquisition probabilities for all components.
+
+    This calculation assumes that correlations for all component codes are performed in
+    parallel. See `[4]`_ Section 2.6.2 for details.
+
+    Parameters
+    ----------
+    ranging_to_noise_psd : Frequency
+        The ranging-to-noise power spectral density :math:`P_R/N_0`.
+    integration_time : Time
+        The integration time :math:`T`.
+    code : PnRangingCode
+        The PN ranging code type.
+
+    Returns
+    -------
+    Dimensionless
+        The overall acquisition probability for the PN ranging code.
+
+    References
+    ----------
+    `[2]`_ Equation (90).
+    """
+    return (
+        float(
+            np.prod(
+                [
+                    pn_component_acquisition_probability(
+                        ranging_to_noise_psd, integration_time, code, component
+                    )
+                    for component in range(2, 7)
+                ]
+            )
+        )
+        * u.dimensionless
+    )
